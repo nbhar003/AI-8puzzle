15219a0a1d1db9dd77be36ad8ae655b1980cb363
neeshabhardwaj <nbhar003@ucr.edu>
2018-10-29 17:37:37 -0700

    Menu correctly working with either default puzzle or user input. Correct puzzle and heuristic passed into the function

--- myprogram.py
+++ myprogram.py
@@ -2,61 +2,77 @@ import numpy as np
 import queue
 
 #the general search code
-goalstate = np.array([[ 1., 2., 3.], [ 4., 5., 6.], [ 7., 8., 0.]])
-print(goalstate[0, 0], goalstate[0, 1], goalstate[0, 2])
-print(goalstate[1, 0], goalstate[1, 1], goalstate[1, 2])
-print(goalstate[2, 0], goalstate[2, 1], goalstate[2, 2])
+#goalstate = [1,2,3,4,5,6,7,8,0]
+goalstate = [[1,2,3],[4,5,6],[7,8,0]]
+#print(goalstate[0, 0], goalstate[0, 1], goalstate[0, 2])
+#print(goalstate[1, 0], goalstate[1, 1], goalstate[1, 2])
+#print(goalstate[2, 0], goalstate[2, 1], goalstate[2, 2])
+
 class gensearch():
     def __init__(initstate, goal, operators):
-        self.initstate = initstate
+        self.initstate = matrix #setting the inital state to what user inputted
         self.goal = goalstate
         self.operators = operators
-        q = queue.Queue()
+        q = queue.PriorityQueue()
+        q.put(initstate)
+        print (q.get())
 
+def search(puzzle, heuristic):
+    print("in search function")
 
-
+    #check where blank is. if blank is not on the outer edge, you can move stuff around
+    #make a set where you add and check if its not inside
 
 #user input starts below
 
 def main(): #the uper input function
-        #print("in main")
-        #print("main line 2")
         print ("Welcome to Bertie Woosters 8-puzzle solver")
+        puzzle = [[1,4,5],[6,7,8],[0,2,3]]
         option = input("Type '1' to use a default puzzle, or '2' to enter your own puzzle:\n")
         print("\nYou have choosen option " + option)
 
         print("Enter your puzzle, use a zero to represent the blank")
         print("\tEnter the first row, use space or tabs between numbers:")
         #row1 = input("\tEnter the first row, use space or tabs between numbers:").split()
-
-        #n=int(input("Enter N for N x N matrix : "))         #3 here
-        matrix=[]                                                #use list for storing 2D array
-
-#get the user input and store it in list (here IN : 1 to 9)
-        for i in range(3):
-            row_list=[]
-            row_list = input().split()         #temporary list to store the row
-            matrix.append(row_list)                               #add the row to the list
-        print(matrix)
-# [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-#Display the 2D array
-        for i in range(3):
-            for j in range(3):
-                print(matrix[i][j], end=" ")
-            print()                                           #new line
-
+        if (option == '1'):
+            print("default puzzle")
+            print(puzzle)
+        elif (option == '2'):
+            puzzle=[]
+            for i in range(3):
+                row_list=[]
+                row_list = input().split()
+                puzzle.append(row_list)
+            print(puzzle)
+        else:
+            print("wrong input")
+            #Displaying the array for testing purpses
+            # for i in range(3):
+            #     for j in range(3):
+            #         print(matrix[i][j], end=" ")
+            #     print()                                           #new line
 
         print("\nEnter your choice of algorithm:")
         algochoice = int(input("\t1. Uniform Cost Search \n\t2. A* with the Misplaced Tile heuristic.\n\t3. A* with the Manhattan distance heuristic.\n"))
 
         if (algochoice == 1):
             print("you choose 1")
-
+            search(puzzle, 0)
         elif (algochoice == 2):
             print("you choose 2")
-        else:
+            heuristic = 0;
+            for i in range(3):
+                for j in range(3):
+                    if (puzzle[i][j] != goalstate[i][j]):
+                        heuristic += 1
+            heuristic -= 1
+            print(heuristic)
+            search(puzzle,heuristic)
+        elif (algochoice == 3):
             print("you choose 3")
+            search(puzzle,)
+        else:
+            print("invalid input")
 
 if __name__ == '__main__':
    main()
